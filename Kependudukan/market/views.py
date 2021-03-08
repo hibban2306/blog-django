b3ad40e13cb4e9a1bd0cebdd35fa0c2d2edb7d26
hilariusperdana <hilarius.perdana@gmail.com>
2021-03-08 09:52:54 +0700

    dng

--- Kependudukan/market/views.py
+++ Kependudukan/market/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from .models import RegBrg
+
+def hapus(req, id):
+    dt = RegBrg.objects.get(id=id)
+    dt.delete()
+    return redirect('/market/')
